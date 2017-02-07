6b8b00b1ddb3630b997f3612c129000d9298bd0e
WindProphet <lihanyuan1996@gmail.com>
2017-02-08 01:08:50 +0800

    1.0

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 server = require('./lib/server.js')
+const fs = require('fs');
+
 port = '52311'
 console.log('Pokémon-GL Rental Team QRCode getter');
 console.log('Source code: https://github.com/WindProphet/pgl-get');
+server.save = __dirname + "/user_file"
+try {
+  savefile = fs.readFileSync(server.save,'utf8')
+  server.user = JSON.parse(savefile)
+} catch (e) {
+  server.user = {}
+}
+
 server.listen(port, () => {
   console.log('Server Start at: http://127.0.0.1:'+port);
   require('opn')('http://127.0.0.1:'+port)

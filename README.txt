818e7be848946c65d3c37caed0b79162db4fe45f
WindProphet <lihanyuan1996@gmail.com>
2017-02-07 23:49:34 +0800

    server

---
+++ README.txt
@@ -0,0 +1,2 @@
+# Pokémon-GL Rental Team QRCode getter
+Source code: https://github.com/WindProphet/pgl-get
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 server = require('./lib/server.js')
 port = '52311'
+console.log('Pokémon-GL Rental Team QRCode getter');
+console.log('Source code: https://github.com/WindProphet/pgl-get');
 server.listen(port, () => {
+  console.log('Server Start at: http://127.0.0.1:'+port);
   require('opn')('http://127.0.0.1:'+port)
 })
